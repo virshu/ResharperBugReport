b0c1e7f38cccdfa0512ae44465e70d02aa2eb73e
virshu <felix@rabinovich.org>
2023-04-20 14:02:40 -0700

    Add project files.

---
+++ DataAoLookupHandler/DataAoLookupHandler.cs
@@ -0,0 +1,22 @@
+﻿using GenerateTables.constants;
+
+namespace DataAoLookupHandler;
+
+public static class DataAoLookupHandler
+{
+    public static readonly Dictionary<int, DataAoLookupHandlerBase> Handlers = new()
+    {
+        {
+            5,
+            new DataAoLookupHandlerBase
+            {
+                InitializeQueryableFunc = context => context.IncarcerationArrestXref.Select(s => new DataAoLookupResult
+                {
+                     RefDetails="[TYPE]: "+ context.Lookup.FirstOrDefault(w=>w.LookupIndex==s.Arrest.ArrestType && w.LookupInterfaceId==LookupConstants.ARRTYPE)!.LookupDescription
+                            + "[DOCKET#]: " + s.Arrest.ArrestCourtDocket + s.Arrest.ArrestArraignmentDate + " [COURT]: "
+                            +context.Agency.FirstOrDefault(w=>w.AgencyId==s.Arrest.ArrestArraignmentCourtId)!.AgencyName
+                })
+            }
+        }
+    };
+}
---
+++ DataAoLookupHandler/DataAoLookupHandlerBase.cs
@@ -0,0 +1,45 @@
+﻿using GenerateTables.Models;
+
+namespace DataAoLookupHandler;
+
+public class DataAoLookupHandlerBase 
+{
+    public readonly Func<IQueryable<DataAoLookupResult>, int, IQueryable<DataAoLookupResult>> AddWhereIdentityFunc =
+        (q, identity) => q.Where(w => w.RefIdentity == identity);
+
+    public readonly Func<IQueryable<DataAoLookupResult>, DateTime, DateTime, IQueryable<DataAoLookupResult>> AddBetweenDatesFunc =
+        (q, dateFrom, dateTo) => q.Where(w => w.RefDateField >= dateFrom && w.RefDateField <= dateTo);
+
+    public readonly Func<IQueryable<DataAoLookupResult>, IQueryable<DataAoLookupResult>> AddWhereNotEmptyFunc =
+        q => q.Where(w => true || string.IsNullOrEmpty(w.RefDetails));
+
+    public readonly Func<IQueryable<DataAoLookupResult>, IQueryable<DataAoLookupResult>> AddOrderByFunc =
+        q => q.OrderBy(o => o.RefOrderBy);
+
+    public readonly Func<IQueryable<DataAoLookupResult>, IQueryable<DataAoLookupResult>> AddOrderByDescFunc =
+        q => q.OrderByDescending(o => o.RefOrderBy);
+
+    public Func<AAtims, IQueryable<DataAoLookupResult>> InitializeQueryableFunc;
+    public Func<AAtims, int, int, int> UpdateEntityFunc;
+}
+
+public class DataAoLookupResult
+{
+    public int RefId { get; set; }
+    public string RefOrderBy { get; set; }
+    public string RefDate { get; set; }
+    public bool RefActive { get; set; }
+    public bool RefSeal { get; set; }
+    public string RefNumber { get; set; }
+    public string RefDetails { get; set; }
+    public int? RefIdentity { get; set; }
+    public DateTime? RefDateField { get; set; }
+    public string RefNoteType { get; set; }
+
+    public DataAoLookupResult()
+    {
+        RefActive = false;
+        RefSeal = false;
+        RefNumber = string.Empty;
+    }
+}
